50048a38574fdf290ca0e60cafd10b8fb874b925
yamd1 <yosky1011@gmail.com>
2023-10-08 22:37:19 +0900

    :memo:remap

--- keyboards/keyball/keyball39/keymaps/default/keymap.c
+++ keyboards/keyball/keyball39/keymaps/default/keymap.c
@@ -20,35 +20,54 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 
+#define LSHIFT_Z MT(MOD_LSFT, KC_Z)
+#define LY3_LNG1 LT(3, KC_LNG1)
+#define LY2_SPC  LT(2, KC_SPC)
+#define LY1_LNG2 LT(1, KC_LNG2)
+#define LY1_BSPC LT(1, KC_BSPC)
+#define LY2_ENT  LT(2, KC_ENT)
+#define CGR      LCTL(LGUI(KC_RIGHT))
+#define CGL      LCTL(LGUI(KC_LEFT))
+#define CG_LOCK  LCTL(LGUI(KC_L))
+#define ALT_SPC  LALT(KC_SPC)
+
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default
   [0] = LAYOUT_universal(
-    KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
-    KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_MINS  ,
-    KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  ,
-    KC_LCTL  , KC_LGUI  , KC_LALT  ,LSFT_T(KC_LNG2),LT(1,KC_SPC),LT(3,KC_LNG1),KC_BSPC,LT(2,KC_ENT),LSFT_T(KC_LNG2),KC_RALT,KC_RGUI, KC_RSFT
-  ),
+    KC_Q     ,  KC_W     ,  KC_E     ,  KC_R     ,  KC_T     ,                               KC_Y     ,  KC_U     ,  KC_I     ,  KC_O     ,  KC_P     ,
+    KC_A     ,  KC_S     ,  KC_D     ,  KC_F     ,  KC_G     ,                               KC_H     ,  KC_J     ,  KC_K     ,  KC_L     ,  KC_SCLN  ,
+    LSHIFT_Z ,  KC_X     ,  KC_C     ,  KC_V     ,  KC_B     ,                               KC_N     ,  KC_M     ,  KC_COMM  ,  KC_DOT   ,  KC_SLSH  ,
+    KC_LCTL  ,  KC_LGUI  ,  KC_LALT  ,  LY3_LNG1 ,  LY2_SPC  ,  LY1_LNG2 ,      LY1_BSPC  ,  LY2_ENT  ,  XXXXXXX  ,  XXXXXXX  ,  XXXXXXX  ,  KC_TAB
+    ),
 
   [1] = LAYOUT_universal(
-    KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_RBRC  ,                            KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   ,
-    KC_F5    , KC_EXLM  , S(KC_6)  ,S(KC_INT3), S(KC_8)  ,                           S(KC_INT1), KC_BTN1  , KC_PGUP  , KC_BTN2  , KC_SCLN  ,
-    S(KC_EQL),S(KC_LBRC),S(KC_7)   , S(KC_2)  ,S(KC_RBRC),                            KC_LBRC  , KC_DLR   , KC_PGDN  , KC_BTN3  , KC_F11   ,
-    KC_INT1  , KC_EQL   , S(KC_3)  , _______  , _______  , _______  ,        TO(2) ,  TO(0)    , _______  , KC_RALT  , KC_RGUI  , KC_F12
-  ),
+    KC_F1    ,  KC_F2    ,  KC_F3    ,  KC_F4    ,  KC_F5    ,                              KC_F6    ,  KC_F7    ,  KC_F8    ,  KC_F9    ,  KC_F10   ,
+    KC_ESC   ,  KC_BTN3  ,  KC_BTN2  ,  KC_BTN1  ,  XXXXXXX  ,                              XXXXXXX  ,  KC_BTN1  ,  KC_BTN2  ,  KC_BTN3  ,  KC_F11   ,
+    _______  ,  XXXXXXX  ,  XXXXXXX  ,  XXXXXXX  ,  XXXXXXX  ,                              XXXXXXX  ,  XXXXXXX  ,  XXXXXXX  ,  XXXXXXX  ,  KC_F12   ,
+    _______  ,  _______  ,  _______  ,  _______  ,  _______  ,  _______  ,      _______  ,  _______  ,  _______  ,  _______  ,  _______  , KC_RIGHT_SHIFT
+    ),
 
   [2] = LAYOUT_universal(
-    KC_TAB   , KC_7     , KC_8     , KC_9     , KC_MINS  ,                            KC_NUHS  , _______  , KC_BTN3  , _______  , KC_BSPC  ,
-   S(KC_QUOT), KC_4     , KC_5     , KC_6     ,S(KC_SCLN),                            S(KC_9)  , KC_BTN1  , KC_UP    , KC_BTN2  , KC_QUOT  ,
-    KC_SLSH  , KC_1     , KC_2     , KC_3     ,S(KC_MINS),                           S(KC_NUHS), KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  ,
-    KC_ESC   , KC_0     , KC_DOT   , KC_DEL   , KC_ENT   , KC_BSPC  ,      _______ ,  _______  , _______  , _______  , _______  , _______
-  ),
+    KC_1     ,  KC_2     ,  KC_3     ,  KC_4     ,  KC_5    ,                               KC_6     ,  KC_7     ,  KC_8     ,  KC_9      ,  KC_0     ,
+    S(KC_1)  ,  S(KC_2)  ,  S(KC_3)  ,  S(KC_4)  ,  S(KC_5) ,                               KC_MINUS ,  KC_EQL   ,  S(KC_9)  ,  S(KC_0)   ,  KC_QUOT  ,
+    _______  ,  C(KC_X)  ,  C(KC_C)  ,  C(KC_V)  ,  C(KC_Z) ,                               XXXXXXX  ,  KC_GRV   ,  KC_LBRC  ,  KC_RBRC   ,  KC_BSLS  ,
+    _______  ,  _______  ,  _______  ,  _______  ,  _______ ,  _______  ,       _______  ,  _______  ,  _______  ,  _______  ,  _______   , _______
+    ),
 
   [3] = LAYOUT_universal(
-    RGB_TOG  , _______  , _______  , _______  , _______  ,                            RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN ,
-    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , SCRL_DVI ,                            RGB_M_K  , RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW ,
-    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , SCRL_DVD ,                            CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE ,
-    QK_BOOT    , KBC_RST  , _______  , _______  , _______  , _______  ,      _______ ,  _______  , _______  , _______  , KBC_RST  , QK_BOOT
+    CG_LOCK  ,  _______  ,  _______  ,  _______  ,  _______  ,                              _______  ,  _______  ,  ALT_SPC  ,  _______  ,  KC_PSCR  ,
+    _______  ,  _______  ,  KC_LCTL  ,  KC_LSFT  ,  _______  ,                              KC_LEFT  ,  KC_DOWN  ,  KC_UP    ,  KC_RIGHT ,  _______  ,
+    _______  ,  _______  ,  _______  ,  _______  ,  _______  ,                              CGL      ,  CGR      ,  _______  ,  _______  ,  _______  ,
+    _______  ,  _______  ,  _______  ,  _______  ,  _______  ,  _______  ,      _______  ,  _______  ,  _______  ,  _______  ,  _______  ,  _______
+  ),
+
+  [4] = LAYOUT_universal(
+    _______  ,  _______  ,  _______  ,  _______  ,  _______  ,                              _______  ,  _______  ,  _______  ,  _______  ,  _______  ,
+    _______  ,  _______  ,  _______  ,  _______  ,  _______  ,                              _______  ,  _______  ,  _______  ,  _______  ,  _______  ,
+    _______  ,  _______  ,  _______  ,  _______  ,  _______  ,                              _______  ,  _______  ,  _______  ,  _______  ,  _______  ,
+    _______  ,  _______  ,  _______  ,  _______  ,  _______  ,  _______  ,      _______  ,  _______  ,  _______  ,  _______  ,  _______  ,  _______
   ),
 };
 // clang-format on
