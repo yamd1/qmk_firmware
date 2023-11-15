2609a7a0e3dae6dd722b624de5594d031c39f53d
yamd1 <yamd1c2d26@gmail.com>
2023-11-15 21:36:38 +0900

    :sparkles: (keyball): DELETEを追加

--- keyboards/keyball/keyball39/keymaps/yamd1/keymap.c
+++ keyboards/keyball/keyball39/keymaps/yamd1/keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F1    ,  KC_F2    ,  KC_F3    ,  KC_F4    ,  KC_F5    ,                              KC_F6    ,  KC_F7    ,  KC_F8    ,  KC_F9    ,  KC_F10   ,
     KC_ESC   ,  KC_BTN3  ,  KC_BTN2  ,  KC_BTN1  ,  XXXXXXX  ,                              KC_LEFT  ,  KC_DOWN  ,  KC_UP    ,  KC_RIGHT ,  KC_F11   ,
     _______  ,  XXXXXXX  ,  XXXXXXX  ,  XXXXXXX  ,  XXXXXXX  ,                              XXXXXXX  ,  CGL      ,  CGR      ,  ALT_SPC  ,  KC_F12   ,
-    _______  ,  _______  ,  _______  ,  _______  ,  _______  ,  _______  ,      _______  ,  _______  ,  _______  ,  _______  ,  _______  , KC_RIGHT_SHIFT
+    _______  ,  _______  ,  _______  ,  KC_DEL   ,  _______  ,  _______  ,      _______  ,  _______  ,  _______  ,  _______  ,  _______  , KC_RIGHT_SHIFT
     ),
 
 
